78bd2a535b98fa898a276e3f38c14f88bfe4fba4
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2024-02-12 11:13:56 +0100

    move docs to a separate module

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ import helper as hp
 import plots as pl
 from anim import animate
 
+import docs
+
 # global variables for csv proximity file
 proximity_results = {}
 proximity_results["path"] = Path("proximity_analysis_results.csv")
@@ -526,63 +528,9 @@ if __name__ == "__main__":
 
     with tab2:
         do_calculations = st.toggle("Activate", key="tab2", value=False)
-        docs = st.expander("Documentation (click to expand)", expanded=False)
-        with docs:
-            st.write(
-                r"""
-                ## Density:
-                The measurement method involves several steps to calculate the individual density based on proximity data. The process is as follows:
-                
-                - Calculate Half Distances: Mathematically, this can be expressed as:
-
-                $$
-                H = \frac{1}{2}  \left( \text{dist}_{\text{prev}} +  \text{dist}_{\text{next}}\right),
-                $$
-
-                where $H$ represents the half sum of distances, $\text{dist}_{\text{prev}}$ and $\text{dist}_{\text{next}}$ denote the Euklidean distances to the previous and next entities, respectively.
-
-                - Calculate Individual Density: The individual density ($\rho$) is then calculated as the inverse of the half sum of distances:
-                $$
-                \rho = \frac{1}{H}
-                $$
-
-                ## Speed
-                The calculation of speed is based on the displacement in the $x$ and $y$ directions over time. The method involves the following steps:
-                - Calculate Displacements: The displacement in both the $x$ and $y$ directions is calculated as the difference between successive positions, accounting for displacement over a specified number of frames ($\Delta t$). This is done separately for each entity, identified by its $id$. The mathematical expressions for these displacements are:
-                """
-            )
-            st.latex(
-                r"""
-                \begin{align*}
-            \Delta x &= x(t + \Delta t) - x(t). \\
-            \Delta y &= y(t + \Delta t) - y(t).
-            \end{align*}
-            """
-            )
-            st.write(
-                """
-                where $\Delta x$ and $\Delta y$ represent the displacements in the $x$ and $y$ directions, respectively, and $\Delta t$ is the difference in frame indices used for the calculation.
-
-                - Compute Distance Traveled: The distance traveled between the frames is computed using the Pythagorean theorem, which combines the displacements in both directions:
-                $$
-                \\text{distance} = \sqrt{\Delta x^2 + \Delta y^2}.
-                $$
-                - Calculate Speed: Finally, the speed is calculated as the ratio of the distance traveled to the time
-                """
-            )
-            st.latex(
-                r"""
-                \begin{equation}
-                \text{speed} = \frac{\text{distance}}{\Delta t}
-                \end{equation}
-                """
-            )
-            st.write(
-                """
-                This yields the speed of each entity between the specified frames, taking into account the displacements in both spatial dimensions.
-                See [implementation here](https://github.com/PedestrianDynamics/gender-experiments/blob/main/analysis.py#L10).
-            """
-            )
+        docs_expander = st.expander("Documentation (click to expand)", expanded=False)
+        with docs_expander:
+            docs.density_speed_documentation()
         c0, c1, c2 = st.columns((1, 1, 1))
         if do_calculations:
             c2.write("**Speed calculation parameters**")
