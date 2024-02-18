0622aa601b759f1a8904e41f9d30fd56151b0097
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2024-02-18 23:57:18 +0100

    Calculate distance for all countries

--- proximity.py
+++ proximity.py
@@ -421,7 +421,7 @@ def init() -> InitData:
     """
     result_csv = Path("app_data/proximity_analysis_results.csv")
     fps = 25  # For distance calculations, calculate every fps-frame
-    countries = ["aus"]  # , "ger", "jap", "chn", "pal"]
+    countries = ["aus", "ger", "jap", "chn", "pal"]
     files = {}
     for country in countries:
         files[country] = [str(path) for path in Path(country).glob("*.csv")]
