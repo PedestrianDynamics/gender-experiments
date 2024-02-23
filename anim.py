416ca7956d139232e9be82974acf37ec2c485ffc
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2024-02-23 19:02:01 +0100

    add color modes to animation

--- anim.py
+++ anim.py
@@ -9,30 +9,37 @@ code here. Use it at your own peril.
 """
 
 
+from typing import List, Tuple, Dict, Any
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
 import pedpy
 import plotly.graph_objects as go
+import streamlit as st
+from plotly.graph_objs import Figure, Scatter
+from plotly.graph_objs.layout import Shape
+from shapely import Polygon
 
 DUMMY_SPEED = -1000
 
 
-def _speed_to_color(speed, min_speed, max_speed):
+def _speed_to_color(speed: float, min_speed: float, max_speed: float) -> str:
     """Map a speed value to a color using a colormap."""
     normalized_speed = (speed - min_speed) / (max_speed - min_speed)
-    r, g, b = plt.cm.jet_r(normalized_speed)[:3]
+    r, g, b = plt.cm.jet_r(normalized_speed)[:3]  # type: ignore
     return f"rgba({r*255:.0f}, {g*255:.0f}, {b*255:.0f}, 0.5)"
 
 
-def _get_line_color(disk_color):
+def _get_line_color(disk_color: str) -> str:
+    """Change line color based on brightness."""
     r, g, b, _ = [int(float(val)) for val in disk_color[5:-2].split(",")]
     brightness = (r * 299 + g * 587 + b * 114) / 1000
     return "black" if brightness > 127 else "white"
 
 
-def _create_orientation_line(row, line_length=0.2, color="black"):
+def _create_orientation_line(row: pd.DataFrame, line_length: float = 0.2, color: str = "black") -> Shape:
+    """Create orientation Shape object."""
     end_x = row["x"] + line_length * 0
     end_y = row["y"] + line_length * 0
 
@@ -46,7 +53,8 @@ def _create_orientation_line(row, line_length=0.2, color="black"):
     )
 
 
-def _get_geometry_traces(area):
+def _get_geometry_traces(area: Polygon) -> Scatter:
+    """Construct geometry traces."""
     geometry_traces = []
     x, y = area.exterior.xy
     geometry_traces.append(
@@ -76,24 +84,34 @@ def _get_geometry_traces(area):
     return geometry_traces
 
 
-def _get_colormap(frame_data, max_speed):
+def _get_colormap(frame_data: pd.DataFrame, max_speed: float, color_mode: str) -> List[Scatter]:
     """Utilize scatter plots with varying colors for each agent instead of individual shapes.
 
     This trace is only to incorporate a colorbar in the plot.
     """
-    scatter_trace = go.Scatter(
-        x=frame_data["x"],
-        y=frame_data["y"],
-        mode="markers",
-        marker={
+    if color_mode == "Speed":
+        marker_dict = {
             "size": frame_data["radius"] * 2,
             "color": frame_data["speed"],
             "colorscale": "Jet_r",
             "colorbar": {"title": "Speed [m/s]"},
             "cmin": 0,
             "cmax": max_speed,
-        },
-        text=frame_data["speed"],
+        }
+    else:
+        colors = frame_data["gender"].map({2: "blue", 1: "green"})
+        marker_dict = {
+            "size": frame_data["radius"] * 2,
+            "color": colors,
+            # "colorbar": {"title": "Gender"},
+        }
+
+    scatter_trace = go.Scatter(
+        x=frame_data["x"],
+        y=frame_data["y"],
+        mode="markers",
+        marker=marker_dict,
+        text=frame_data["speed"] if color_mode == "speed" else frame_data["gender"],
         showlegend=False,
         hoverinfo="none",
     )
@@ -101,8 +119,11 @@ def _get_colormap(frame_data, max_speed):
     return [scatter_trace]
 
 
-def _get_shapes_for_frame(frame_data, min_speed, max_speed):
-    def create_shape(row):
+def _get_shapes_for_frame(frame_data: pd.DataFrame, min_speed: float, max_speed: float, color_mode: str) -> Tuple[Shape, Scatter, Shape]:
+    """Construct circles as Shapes for agents, Hover and Directions."""
+
+    def create_shape(row: pd.DataFrame) -> Shape:
+        """Construct circles as Shapes for agents."""
         hover_trace = go.Scatter(
             x=[row["x"]],
             y=[row["y"]],
@@ -136,15 +157,16 @@ def _get_shapes_for_frame(frame_data, min_speed, max_speed):
                 dummy_trace,
                 _create_orientation_line(row, color="rgba(255,255,255,0)"),
             )
-        # color = _speed_to_color(row["speed"], min_speed, max_speed)
-        gender_colors = {
-            1: "blue",  # Assuming 1 is for female
-            2: "green",  # Assuming 2 is for male
-            0: "black",  # non binary
-            -1: "yellow",
-        }
-
-        color = gender_colors[row["gender"]]
+        if color_mode == "Speed":
+            color = _speed_to_color(row["speed"], min_speed, max_speed)
+        else:
+            gender_colors = {
+                1: "blue",  # Assuming 1 is for female
+                2: "green",  # Assuming 2 is for male
+                0: "black",  # non binary
+                -1: "yellow",
+            }
+            color = gender_colors[row["gender"]]
         return (
             go.layout.Shape(
                 type="circle",
@@ -162,6 +184,7 @@ def _get_shapes_for_frame(frame_data, min_speed, max_speed):
         )
 
     results = frame_data.apply(create_shape, axis=1).tolist()
+    # results = frame_data.apply(lambda row: create_shape(color_mode=color_mode), axis=1).tolist()
     shapes = [res[0] for res in results]
     hover_traces = [res[1] for res in results]
     arrows = [res[2] for res in results]
@@ -169,19 +192,19 @@ def _get_shapes_for_frame(frame_data, min_speed, max_speed):
 
 
 def _create_fig(
-    initial_agent_count,
-    initial_shapes,
-    initial_arrows,
-    initial_hover_trace,
-    initial_scatter_trace,
-    geometry_traces,
-    frames,
-    steps,
-    area_bounds,
-    width=800,
-    height=800,
+    initial_agent_count: int,
+    initial_shapes: Shape,
+    initial_arrows: Shape,
+    initial_hover_trace: Shape,
+    initial_scatter_trace: Shape,
+    geometry_traces: Shape,
+    frames: pd.DataFrame,
+    steps: List[Dict[str, Any]],
+    area_bounds: Tuple[float, float, float, float],
+    width: int = 800,
+    height: int = 800,
     title_note: str = "",
-):
+) -> Figure:
     """Creates a Plotly figure with animation capabilities.
 
     Returns:
@@ -191,14 +214,9 @@ def _create_fig(
     minx, miny, maxx, maxy = area_bounds
     title = f"<b>{title_note + '  |  ' if title_note else ''}Number of Agents: {initial_agent_count}</b>"
     fig = go.Figure(
-        data=geometry_traces
-        # + initial_scatter_trace
-        # + hover_traces
-        + initial_hover_trace,
+        data=geometry_traces + initial_scatter_trace + initial_hover_trace,
         frames=frames,
-        layout=go.Layout(
-            shapes=initial_shapes + initial_arrows, title=title, title_x=0.5
-        ),
+        layout=go.Layout(shapes=initial_shapes + initial_arrows, title=title, title_x=0.5),
     )
     fig.update_layout(
         updatemenus=[_get_animation_controls()],
@@ -213,7 +231,7 @@ def _create_fig(
     return fig
 
 
-def _get_animation_controls():
+def _get_animation_controls() -> Dict[str, Any]:
     """Returns the animation control buttons for the figure."""
     return {
         "buttons": [
@@ -240,7 +258,7 @@ def _get_animation_controls():
     }
 
 
-def _get_slider_controls(steps):
+def _get_slider_controls(steps: List[Dict[str, Any]]) -> Dict[str, Any]:
     """Returns the slider controls for the figure."""
     return {
         "active": 0,
@@ -261,7 +279,7 @@ def _get_slider_controls(steps):
     }
 
 
-def _get_processed_frame_data(data_df, frame_num, max_agents):
+def _get_processed_frame_data(data_df: pd.DataFrame, frame_num: int, max_agents: int) -> Tuple[pd.DataFrame, int]:
     """Process frame data and ensure it matches the maximum agent count."""
     frame_data = data_df[data_df["frame"] == frame_num]
     agent_count = len(frame_data)
@@ -273,22 +291,19 @@ def _get_processed_frame_data(data_df, frame_num, max_agents):
 
 
 def animate(
-    data: pedpy.TrajectoryData,
+    data_df: pd.DataFrame,
     area: pedpy.WalkableArea,
+    color_mode,
     *,
-    every_nth_frame: int = 50,
     width: int = 800,
     height: int = 800,
     radius: float = 0.1,
+    every_nth_frame: int = 50,
     title_note: str = "",
-):
-    data_df = pedpy.compute_individual_speed(
-        traj_data=data,
-        frame_step=5,
-        # speed_calculation=pedpy.SpeedCalculation.BORDER_SINGLE_SIDED,
-    )
-    data_df = data_df.merge(data.data, on=["id", "frame"], how="left")
+) -> Figure:
+    """Animate a trajectory."""
     data_df["radius"] = radius
+    frames = data_df["frame"].unique()
     min_speed = data_df["speed"].min()
     max_speed = data_df["speed"].max()
     max_agents = data_df.groupby("frame").size().max()
@@ -303,16 +318,13 @@ def animate(
         initial_shapes,
         initial_hover_trace,
         initial_arrows,
-    ) = _get_shapes_for_frame(initial_frame_data, min_speed, max_speed)
-    color_map_trace = _get_colormap(initial_frame_data, max_speed)
+    ) = _get_shapes_for_frame(initial_frame_data, min_speed, max_speed, color_mode)
+    color_map_trace = _get_colormap(initial_frame_data, max_speed, color_mode)
     for frame_num in selected_frames:
-        frame_data, agent_count = _get_processed_frame_data(
-            data_df, frame_num, max_agents
-        )
-        shapes, hover_traces, arrows = _get_shapes_for_frame(
-            frame_data, min_speed, max_speed
-        )
-        title = f"<b>{title_note + '  |  ' if title_note else ''}N: {agent_count}</b>"
+        frame_data, agent_count = _get_processed_frame_data(data_df, frame_num, max_agents)
+        shapes, hover_traces, arrows = _get_shapes_for_frame(frame_data, min_speed, max_speed, color_mode)
+        # title = f"<b>{title_note + '  |  ' if title_note else ''}N: {agent_count}</b>"
+        title = f"<b>{title_note + '  |  ' if title_note else ''}Number of Agents: {initial_agent_count}</b>"
         frame_name = str(int(frame_num))
         frame = go.Frame(
             data=geometry_traces + hover_traces,
